ad7f7b5b75ef3fb214838aac6d21f2db01ff01dc
mhaikalmusyaffa <148994145+mhaikalmusyaffa@users.noreply.github.com>
2023-10-26 21:34:47 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -3,6 +3,6 @@
 
 // TODO: change this
 #define TargetLibName "libil2cpp.so"
-#define GamePackageName "com.garena.game.codm"
+#define GamePackageName "com.miniclip.agar.io"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
